1bd5294942e2f1bb4febddef55dc74a28ba4c1cf
daprett <athallahdaffa936@gmail.com>
2026-05-04 15:40:34 +0700

    add: adminSeeder

---
+++ Rekomendasi_magang/database/seeders/AdminSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('admin')->insert([
+            [
+                'name' => 'Super Admin',
+                'email' => 'admin@example.com',
+                'password' => Hash::make('password123'),
+                'role' => 'admin',
+                'remember_token' => null,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]
+        ]);
+    }
+}
