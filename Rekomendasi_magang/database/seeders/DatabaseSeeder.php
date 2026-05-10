16b033dfc6d19fa2c9cc9d0201a6bfa80498a872
daprett <athallahdaffa936@gmail.com>
2026-05-10 20:47:08 +0700

    Update: file databaseSeeder

--- Rekomendasi_magang/database/seeders/DatabaseSeeder.php
+++ Rekomendasi_magang/database/seeders/DatabaseSeeder.php
@@ -21,7 +21,10 @@ class DatabaseSeeder extends Seeder
         PerusahaanSeeder::class,
         MinatBidangSeeder::class,
         TechnologySeeder::class,
+        SkillSeeder::class,
         perusahaan_posisipvtSeeder::class,
+        perusahaan_technologypvtSeeder::class,
+        
     ]);
 
         User::factory()->create([
--- Rekomendasi_magang/database/seeders/perusahaan_technologypvtSeeder.php
+++ Rekomendasi_magang/database/seeders/perusahaan_technologypvtSeeder.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-class perusahaan_teknologypvtSeeder extends Seeder
+class perusahaan_technologypvtSeeder extends Seeder
 {
     /**
      * Run the database seeds.
