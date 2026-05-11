4951b48c0eb775e8a8b64e800bc718a8cce93de9
annisaagstn <annisaagustina84@gmail.com>
2026-05-11 15:59:18 +0700

    add:frontend form_page

--- Rekomendasi_magang/routes/web.php
+++ Rekomendasi_magang/routes/web.php
@@ -7,15 +7,22 @@ Route::get('/', function () {
     return view('admin.login');
 });
 
+Route::get('/detail-perusahaan', function () {
+    return view('mahasiswa.detail_perusahaan');
+});
+
+Route::get('/form-page', function () {
+    return view('mahasiswa.form_page');
+});
+
 Route::post('/login', function (Request $request) {
-    // ambil input
+
     $email = $request->email;
     $password = $request->password;
 
-    // sementara (dummy login)
     if ($email == 'admin@gmail.com' && $password == '123456') {
-        return "Login berhasil";
+        return redirect('/detail-perusahaan');
     }
 
-    return back()->with('error', 'Email atau password salah');
+    return back()->with('error', 'Email atau Password salah');
 })->name('login');
