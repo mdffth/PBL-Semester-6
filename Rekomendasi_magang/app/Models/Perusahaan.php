92249459dbe6d613909fe2ba0c7e5fcff2d21f1f
dhynarazarine <dinar.andiyani@gmail.com>
2026-05-18 17:50:52 +0700

    add : landing page

--- Rekomendasi_magang/app/Models/Perusahaan.php
+++ Rekomendasi_magang/app/Models/Perusahaan.php
@@ -11,15 +11,17 @@ class Perusahaan extends Model
 {
     use HasFactory;
 
+     protected $table = 'perusahaan';
+
     protected $fillable = [
-        'nama_perusahaan',
-        'profil_perusahaan',
-        'bidang_industri',
-        'posisi_magang',
-        'minimal_ipk',
-        'job_description',
-        'durasi_magang',
-        'status_magang',
+    'name',
+    'profile_perusahaan',
+    'tipe_industri',
+    'posisi_magang',
+    'min_ipk',
+    'job_description',
+    'duration_months',
+    'status_magang',
     ];
 
     protected $casts = [
