9afc189212ce1a43799d8f4d4a90f237752f06f9
daprett <athallahdaffa936@gmail.com>
2026-06-04 20:25:48 +0700

    update: seeder

--- Rekomendasi_magang/database/seeders/UlasanSeeder.php
+++ Rekomendasi_magang/database/seeders/UlasanSeeder.php
@@ -17,37 +17,37 @@ class UlasanSeeder extends Seeder
         DB::table('ulasan')->insert([
                 [
                     'name' => 'Daffa',
-                    'position' => 'PT ARM Solusi',
+                    'position' => '7',
                     'rating' => '4',
                     'review' => 'Saya merasa sangat terbantu dengan RekomIn. Platform ini membantu saya menemukan tempat magang yang sesuai dengan skill dan minat saya.',
                 ],
                 [
                     'name' => 'Isna',
-                    'position' => 'PT Link Apisindo',
+                    'position' => '6',
                     'rating' => '5',
                     'review' => 'Fitur rekomendasinya akurat banget! Tidak perlu bingung pilih tempat magang karena sistem langsung mencarikan yang paling cocok untuk saya.',
                 ],
                 [
                     'name' => 'Zaki',
-                    'position' => 'Dinas Kominfo Jatim',
+                    'position' => '7',
                     'rating' => '4',
                     'review' => 'Tampilan webnya bersih dan mudah digunakan. Informasi perusahaan lengkap, mulai dari posisi sampai info uang saku. Sangat recommended!',
                 ],
                 [
                     'name' => 'Rara',
-                    'position' => 'Sarastya Innovations',
+                    'position' => '6',
                     'rating' => '5',
                     'review' => 'Awalnya bingung mau magang di mana, tapi setelah isi form di RekomIn langsung dapet 5 rekomendasi relevan. Proses seleksinya jadi lebih terarah!',
                 ],
                 [
                     'name' => 'Faiz',
-                    'position' => 'Timedoor Academy',
+                    'position' => '6',
                     'rating' => '5',
                     'review' => 'Skor kesesuaiannya membantu banget buat nentuin prioritas. Saya pilih yang 95% match dan ternyata emang cocok banget sama ritme kerja saya',
                 ],
                 [
                     'name' => 'Aldi',
-                    'position' => 'DLH Kota Surabaya',
+                    'position' => '6',
                     'rating' => '5',
                     'review' => 'Sistem ini membantu saya yang masih semester 6 buat tahu perusahaan mana yang cocok sama IPK dan skill saya. Jadi lebih percaya diri waktu apply!',
             ],
